03fefba2622b70fec73865fdb0352ed4cb829ab5
unknown <>
2016-06-15 03:37:52 +0800

    Initial commit

---
+++ library/src/main/java/com/example/library/MyNumberPrassbar.java
@@ -0,0 +1,19 @@
+package com.example.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by cys on 2016/6/15.
+ */
+public class MyNumberPrassbar extends View {
+    public MyNumberPrassbar(Context context) {
+        this(context,null);
+    }
+
+    public MyNumberPrassbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
